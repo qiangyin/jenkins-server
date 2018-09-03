89b49ea2a2ec0a868ac0546d7aecb79737236ac3
yintongqiang <tongqiang.yin@dmall.com>
2018-09-03 11:35:17 +0800

    update

--- src/test/java/com/cloudzone/service/JenkinsJenkinsServiceApiImplTest.java
+++ src/test/java/com/cloudzone/service/JenkinsJenkinsServiceApiImplTest.java
@@ -22,11 +22,11 @@ public class JenkinsJenkinsServiceApiImplTest {
         jobConfig = new JobConfig();
         jobConfig.setJobName("test" + System.currentTimeMillis());
         jobConfig.setCodeBranch("master");
-        jobConfig.setCodeUrl("http://10.112.101.94/cloudzone/autotest.git/");
-        jobConfig.setBaseImage("10.112.101.90/cloudzone/alpine-sunjdk8:151");
+        jobConfig.setCodeUrl("http://192.168.101.94/cloudzone/autotest.git/");
+        jobConfig.setBaseImage("192.168.101.90/cloudzone/alpine-sunjdk8:151");
         jobConfig.setImageUser("gc-cloud");
         jobConfig.setImageName("autotest:3.2");
-        jobConfig.setRepositoryUrl("10.112.101.90");
+        jobConfig.setRepositoryUrl("192.168.101.90");
         jobConfig.setRepositoryUsername("admin");
         jobConfig.setRepositoryPassword("Harbor12345");
         jobConfig.setRepositoryName("cloudzone");
